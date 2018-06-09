22c84edd52b2ad123f01e1fd4934a3f3313f6f03
Jeremy Hicks <jerhicks@cisco.com>
2018-06-09 09:01:25 +0100

    Merge 'add-tests' branch

---
+++ src/test/java/uk/org/thehickses/channel/ChannelTest.java
@@ -0,0 +1,72 @@
+package uk.org.thehickses.channel;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+
+public class ChannelTest
+{
+    @Test
+    public void testSingleSupplier()
+    {
+        testChannel(1);
+    }
+
+    @Test
+    public void testMultipleSuppliers()
+    {
+        testChannel(90);
+    }
+
+    public void testChannel(int supplierThreads)
+    {
+        Channel<Integer> channel = new Channel<>();
+        Channel<Integer> putsAndGets = new Channel<>(supplierThreads + 1);
+        AtomicInteger putCount = new AtomicInteger();
+        int valueCount = 2000;
+        for (int i = 0; i < supplierThreads; i++)
+        {
+            int t = i;
+            Runnable putter = () -> {
+                int count = 0;
+                for (int v = t; v < valueCount; v += supplierThreads)
+                {
+                    channel.put(v);
+                    count++;
+                }
+                putsAndGets.put(count);
+                putCount.addAndGet(count);
+            };
+            new Thread(putter).start();
+        }
+        List<Integer> values = new ArrayList<Integer>();
+        Runnable reader = () -> {
+            channel.range(v -> {
+                values.add(v);
+                putsAndGets.put(-1);
+            });
+        };
+        new Thread(reader).start();
+        int outstandingValueCount = Integer.MIN_VALUE;
+        while (outstandingValueCount != 0)
+        {
+            GetResult<Integer> result = putsAndGets.get();
+            if (outstandingValueCount == Integer.MIN_VALUE)
+                outstandingValueCount = result.value;
+            else
+                outstandingValueCount += result.value;
+        }
+        channel.close();
+        assertThat(values.size()).isEqualTo(valueCount);
+        assertThat(putCount.get()).isEqualTo(valueCount);
+        Collections.sort(values);
+        for (int i = 0; i < valueCount; i++)
+            assertThat(values.get(i)).isEqualTo(i);
+    }
+
+}
