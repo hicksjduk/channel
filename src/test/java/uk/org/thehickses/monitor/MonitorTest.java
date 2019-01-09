f11d8207a10b23d6c0bd593db2fa658e6aa4c1bf
Jeremy Hicks <jerhicks@cisco.com>
2019-01-09 13:32:20 +0000

    Remove delays, and increase load

--- src/test/java/uk/org/thehickses/monitor/MonitorTest.java
+++ src/test/java/uk/org/thehickses/monitor/MonitorTest.java
@@ -3,7 +3,6 @@ package uk.org.thehickses.monitor;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import org.junit.Test;
@@ -17,20 +16,21 @@ public class MonitorTest
     @Test
     public void test()
     {
+        int threadCount = 100;
         Monitor monitor = new Monitor();
         Channel<Void>[] channels = IntStream
-                .range(0, 10)
+                .range(0, threadCount)
                 .mapToObj(i -> new Channel<Void>())
                 .toArray(Channel[]::new);
-        Arrays.stream(channels, 0, 5).forEach(ch -> runProcess(monitor, ch));
+        Arrays.stream(channels, 0, threadCount / 2).forEach(ch -> runProcess(monitor, ch));
         Channel<Void> doneChannel1 = startWait(monitor);
-        Arrays.stream(channels, 5, 10).forEach(ch -> runProcess(monitor, ch));
+        Arrays.stream(channels, threadCount / 2, threadCount).forEach(ch -> runProcess(monitor, ch));
         Channel<Void> doneChannel2 = startWait(monitor);
-        Arrays.stream(channels, 0, 5).forEach(this::waitAndClose);
+        Arrays.stream(channels, 0, threadCount / 2).forEach(Channel::close);
         doneChannel1.get();
         assertThat(doneChannel2.isOpen());
         Channel<Void> doneChannel3 = startWait(monitor);
-        Arrays.stream(channels, 5, 10).forEach(this::waitAndClose);
+        Arrays.stream(channels, threadCount / 2, threadCount).forEach(Channel::close);
         doneChannel2.get();
         doneChannel3.get();
     }
@@ -51,18 +51,4 @@ public class MonitorTest
         }).start();
         return answer;
     }
-
-    private <T> void waitAndClose(Channel<T> ch)
-    {
-        int seconds = 1;
-        try
-        {
-            Thread.sleep(TimeUnit.SECONDS.toMillis(seconds));
-        }
-        catch (InterruptedException e)
-        {
-        }
-        ch.close();
-    }
-
 }
