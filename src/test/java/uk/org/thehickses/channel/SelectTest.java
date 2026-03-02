dea2f8c3dde5e62c843dcd12f15d878293864b74
Jeremy Hicks <jeremy@thehickses.org.uk>
2026-03-02 10:39:10 +0000

    Refactor

--- src/test/java/uk/org/thehickses/channel/SelectTest.java
+++ src/test/java/uk/org/thehickses/channel/SelectTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterEach;
@@ -16,65 +17,69 @@ import uk.org.thehickses.channel.Select.Selecter;
 @SuppressWarnings("unchecked")
 public class SelectTest
 {
-    private Channel<Integer> ch1 = new Channel<>(5);
-    private Channel<Boolean> ch2 = new Channel<>(5);
-    private Channel<String> ch3 = new Channel<>(5);
-    private Consumer<Integer> proc1 = mock(Consumer.class);
-    private Consumer<Boolean> proc2 = mock(Consumer.class);
-    private Consumer<String> proc3 = mock(Consumer.class);
-    private Runnable defaultProc = mock(Runnable.class);
+    private Channel<Integer> channel1 = new Channel<>(5);
+    private Channel<Boolean> channel2 = new Channel<>(5);
+    private Channel<String> channel3 = new Channel<>(5);
+    private Consumer<Integer> handler1 = mock(Consumer.class);
+    private Consumer<Boolean> handler2 = mock(Consumer.class);
+    private Consumer<String> handler3 = mock(Consumer.class);
+    private Runnable defaultHandler = mock(Runnable.class);
 
     @AfterEach
     public void tearDown()
     {
-        verifyNoMoreInteractions(proc1, proc2, proc3, defaultProc);
+        verifyNoMoreInteractions(handler1, handler2, handler3, defaultHandler);
     }
 
     @Test
     public void testWithSinglePut1()
     {
-        testWithSinglePut(ch1, proc1, 42);
+        testWithSinglePut(channel1, handler1, 42);
     }
 
     @Test
     public void testWithSinglePut2()
     {
-        testWithSinglePut(ch2, proc2, true);
+        testWithSinglePut(channel2, handler2, true);
     }
 
     @Test
     public void testWithSinglePut3()
     {
-        testWithSinglePut(ch3, proc3, "Hello");
+        testWithSinglePut(channel3, handler3, "Hello");
     }
 
     private <T> void testWithSinglePut(Channel<T> channel, Consumer<T> receiver, T value)
     {
         channel.put(value);
-        assertThat(Select.withCase(ch1, proc1).withCase(ch2, proc2).withCase(ch3, proc3).run())
-                .isTrue();
+        assertThat(Select.withCase(channel1, handler1)
+                .withCase(channel2, handler2)
+                .withCase(channel3, handler3)
+                .run()).isTrue();
         verify(receiver).accept(value);
     }
 
     @Test
     public void testWithMultiplePut()
     {
-        ch3.put("Bonjour");
-        ch1.put(981);
-        ch2.put(false);
-        ch3.put("Hej");
+        channel3.put("Bonjour");
+        channel1.put(981);
+        channel2.put(false);
+        channel3.put("Hej");
         var verifiers = new ArrayList<Runnable>();
-        doAnswer(invocation -> verifiers.add(() -> verify(proc1).accept(invocation.getArgument(0))))
-                .when(proc1)
-                .accept(anyInt());
-        doAnswer(invocation -> verifiers.add(() -> verify(proc2).accept(invocation.getArgument(0))))
-                .when(proc2)
-                .accept(any());
-        doAnswer(invocation -> verifiers.add(() -> verify(proc3).accept(invocation.getArgument(0))))
-                .when(proc3)
-                .accept(any());
-        assertThat(Select.withCase(ch1, proc1).withCase(ch2, proc2).withCase(ch3, proc3).run())
-                .isTrue();
+        doAnswer(invocation -> verifiers
+                .add(() -> verify(handler1).accept(invocation.getArgument(0)))).when(handler1)
+                        .accept(anyInt());
+        doAnswer(invocation -> verifiers
+                .add(() -> verify(handler2).accept(invocation.getArgument(0)))).when(handler2)
+                        .accept(any());
+        doAnswer(invocation -> verifiers
+                .add(() -> verify(handler3).accept(invocation.getArgument(0)))).when(handler3)
+                        .accept(any());
+        assertThat(Select.withCase(channel1, handler1)
+                .withCase(channel2, handler2)
+                .withCase(channel3, handler3)
+                .run()).isTrue();
         assertThat(verifiers.size()).isEqualTo(1);
         verifiers.forEach(Runnable::run);
     }
@@ -82,79 +87,82 @@ public class SelectTest
     @Test
     public void testWithMultiplePutAndGet()
     {
-        ch3.put("Bonjour");
-        ch1.put(981);
-        ch2.put(false);
-        ch3.put("Hej");
-        var select = Select.withCase(ch1, proc1).withCase(ch2, proc2).withCase(ch3, proc3);
-        for (var count = 0; count < 4; count++)
-            assertThat(select.run()).isTrue();
-        verify(proc1).accept(981);
-        verify(proc2).accept(false);
-        verify(proc3).accept("Bonjour");
-        verify(proc3).accept("Hej");
+        channel3.put("Bonjour");
+        channel1.put(981);
+        channel2.put(false);
+        channel3.put("Hej");
+        var select = Select.withCase(channel1, handler1)
+                .withCase(channel2, handler2)
+                .withCase(channel3, handler3);
+        IntStream.range(0, 4)
+                .forEach(i -> assertThat(select.run()).isTrue());
+        verify(handler1).accept(981);
+        verify(handler2).accept(false);
+        verify(handler3).accept("Bonjour");
+        verify(handler3).accept("Hej");
     }
 
     @Test
     public void testWithDefault()
     {
-        assertThat(Select
-                .withCase(ch1, proc1)
-                .withCase(ch2, proc2)
-                .withCase(ch3, proc3)
-                .withDefault(defaultProc)
+        assertThat(Select.withCase(channel1, handler1)
+                .withCase(channel2, handler2)
+                .withCase(channel3, handler3)
+                .withDefault(defaultHandler)
                 .run()).isTrue();
-        verify(defaultProc).run();
+        verify(defaultHandler).run();
     }
 
     @Test
     public void testAllClosedNoDefault()
     {
-        Stream.of(ch1, ch2, ch3).forEach(Channel::close);
-        assertThat(Select.withCase(ch1, proc1).withCase(ch2, proc2).withCase(ch3, proc3).run())
-                .isFalse();
+        Stream.of(channel1, channel2, channel3)
+                .forEach(Channel::close);
+        assertThat(Select.withCase(channel1, handler1)
+                .withCase(channel2, handler2)
+                .withCase(channel3, handler3)
+                .run()).isFalse();
     }
 
     @Test
     public void testWithDefaultAndTwoPuts()
     {
-        ch2.put(false);
-        ch3.put("Hello");
-        Selecter select = Select
-                .withCase(ch1, proc1)
-                .withCase(ch2, proc2)
-                .withCase(ch3, proc3)
-                .withDefault(defaultProc);
+        channel2.put(false);
+        channel3.put("Hello");
+        Selecter select = Select.withCase(channel1, handler1)
+                .withCase(channel2, handler2)
+                .withCase(channel3, handler3)
+                .withDefault(defaultHandler);
         assertThat(select.run()).isTrue();
         assertThat(select.run()).isTrue();
         assertThat(select.run()).isTrue();
-        verify(proc2).accept(false);
-        verify(proc3).accept("Hello");
-        verify(defaultProc).run();
+        verify(handler2).accept(false);
+        verify(handler3).accept("Hello");
+        verify(defaultHandler).run();
     }
 
     @Test
     public void testAllClosedWithDefault()
     {
-        Stream.of(ch1, ch2, ch3).forEach(Channel::close);
-        assertThat(Select
-                .withCase(ch1, proc1)
-                .withCase(ch2, proc2)
-                .withCase(ch3, proc3)
-                .withDefault(defaultProc)
+        Stream.of(channel1, channel2, channel3)
+                .forEach(Channel::close);
+        assertThat(Select.withCase(channel1, handler1)
+                .withCase(channel2, handler2)
+                .withCase(channel3, handler3)
+                .withDefault(defaultHandler)
                 .run()).isFalse();
     }
 
     @Test
     public void testAllButOneClosedWithDefault()
     {
-        Stream.of(ch1, ch3).forEach(Channel::close);
-        assertThat(Select
-                .withCase(ch1, proc1)
-                .withCase(ch2, proc2)
-                .withCase(ch3, proc3)
-                .withDefault(defaultProc)
+        Stream.of(channel1, channel3)
+                .forEach(Channel::close);
+        assertThat(Select.withCase(channel1, handler1)
+                .withCase(channel2, handler2)
+                .withCase(channel3, handler3)
+                .withDefault(defaultHandler)
                 .run()).isTrue();
-        verify(defaultProc).run();
+        verify(defaultHandler).run();
     }
 }
